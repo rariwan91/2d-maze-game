52ba9dc52f8123991bb32d24aab1543ab8fe9ec8
James Couch <j.rariwan91@gmail.com>
2020-10-22 22:52:36 -0500

    added electron specific index html page

--- src/electronMain.ts
+++ src/electronMain.ts
@@ -10,7 +10,7 @@ function createWindow() {
         }
     })
 
-    win.loadFile(path.join(__dirname, "../../index.html"));
+    win.loadFile(path.join(__dirname, "../../index-electron.html"));
     win.webContents.openDevTools()
 }
 
